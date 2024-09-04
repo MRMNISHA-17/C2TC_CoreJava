b6543c3a49e688dc442dee52d504f81be894d5c8
LENOVO <LENOVO@NISHA>
2024-09-04 14:44:43 +0530

    this is my third commit

---
+++ c2tc_NISHANTHI_M/src/com/TNS/day2/ThirdProgram.java
@@ -0,0 +1,30 @@
+package com.TNS.day2;
+
+public class ThirdProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Loop in java
+		// while loop
+		int i=0,j=0,k=0;
+	    while (i<=10)
+	    {
+	        System.out.println(i);
+	        i++;
+	    }
+	    // do while
+	    do
+	     {
+	        System.out.println(j);
+	        j++;
+	     }while(j<=15);
+	    
+	    // for loop
+	    for (int k=0;k<=10;k++){
+	         System.out.println(k);
+	    }
+	    
+
+	}
+
+}
